a5e11a9a4663845531ebe280f31dae14640fc259
Matthias Reumann <matthias.reumann@tum.de>
2023-06-09 08:45:41 +0200

    Add benchmark for complex contraction

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <complex>
 #include "tfctc.hpp"
 
-int main()
-{
+void benchmark() {
   const int N = 1000;
   float *A = nullptr, *B = nullptr, *C = nullptr;
 
@@ -28,11 +28,52 @@ int main()
     }
 
     std::cout << "TEST: d1=" << d1 << " d2=" << d2 << " d3=" << d3 << std::endl;
-    std::cout << "Minimum: " << *min_element(time.begin(), time.end()) << " ms" << std::endl;
-    std::cout << "Average: " << accumulate(time.begin(), time.end(), 0.0) * 1.0 / N << " ms" << std::endl;
+    std::cout << "Minimum: " << *min_element(time.begin(), time.end()) << " μs" << std::endl;
+    std::cout << "Average: " << accumulate(time.begin(), time.end(), 0.0) * 1.0 / N << " μs" << std::endl;
 
     free(A);
     free(B);
     free(C);
   }
+}
+
+void benchmark_complex() {
+  const int N = 1000;
+  std::complex<float> *A = nullptr, *B = nullptr, *C = nullptr;
+
+  for (int i = 0; i < 11; i++)
+  {
+    int d1 = pow(2, i), d2 = pow(2, i), d3 = pow(2, i);
+
+    alloc_aligned(&A, d1 * d2);
+    alloc_aligned(&B, d2 * d3);
+    alloc_aligned(&C, d1 * d3);
+
+    auto tensorA = Tensor<std::complex<float>>({d1, d2}, A);
+    auto tensorB = Tensor<std::complex<float>>({d2, d3}, B);
+    auto tensorC = Tensor<std::complex<float>>({d1, d3}, C);
+
+    std::vector<float> time(N);
+    for (uint i = 0; i < N; i++)
+    {
+      auto t0 = std::chrono::high_resolution_clock::now();
+      contract(tensorA, "ab", tensorB, "cb", tensorC, "ac");
+      auto t1 = std::chrono::high_resolution_clock::now();
+      time[i] = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+    }
+
+    std::cout << "TEST: d1=" << d1 << " d2=" << d2 << " d3=" << d3 << std::endl;
+    std::cout << "Minimum: " << *min_element(time.begin(), time.end()) << " μs" << std::endl;
+    std::cout << "Average: " << accumulate(time.begin(), time.end(), 0.0) * 1.0 / N << " μs" << std::endl;
+
+    free(A);
+    free(B);
+    free(C);
+  }
+}
+
+int main()
+{
+  // benchmark();
+  benchmark_complex();
 }
