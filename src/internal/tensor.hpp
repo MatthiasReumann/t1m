d13d5ea6d5bcf8a8b441a267739106967930a34e
Matthias Reumann <matthias.reumann@tum.de>
2023-06-22 09:09:11 +0200

    Overload tensor.hpp constructor

--- src/internal/tensor.hpp
+++ src/internal/tensor.hpp
@@ -8,7 +8,9 @@ namespace tfctc
   class Tensor : public MArray::marray_view<T>
   {
   public:
-    Tensor(std::initializer_list<int> lengths, T *ptr)
+    Tensor(std::initializer_list<size_t> lengths, T *ptr)
+        : MArray::marray_view<T>(lengths, ptr, MArray::COLUMN_MAJOR) {}
+    Tensor(std::vector<size_t> lengths, T *ptr)
         : MArray::marray_view<T>(lengths, ptr, MArray::COLUMN_MAJOR) {}
   };
 };
