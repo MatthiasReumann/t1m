a48be18f834bbbcbd6bd481160d95d527fdae450
Matthias Reumann <matthias.reumann@tum.de>
2023-06-25 17:41:09 +0200

    Update benchmark

--- benchmark/benchmark_complex.cpp
+++ benchmark/benchmark_complex.cpp
@@ -3,8 +3,7 @@
 #include <complex>
 #include "tfctc/tfctc.hpp"
 
-const int N = 16;
-const int POWER = 12;
+const int N = 10;
 
 template<typename T>
 void set_random(std::complex<T> *tensor, size_t size)
@@ -18,45 +17,44 @@ void set_random(std::complex<T> *tensor, size_t size)
 
 int main()
 {
-  size_t d1, d2, d3;
   std::complex<float>* A = nullptr, * B = nullptr, * C = nullptr;
   
-  std::cout << "d1=d2=d3;min(μs);avg(μs)"<< '\n';
-  for (int i = 10; i < 11; i++)
+  std::cout << "m=n=k;min(μs);avg(μs)"<< '\n';
+
+  size_t workspace_size = 62l * 62l * 62l * 62l;
+  tfctc::utils::alloc_aligned(&A, workspace_size);
+  tfctc::utils::alloc_aligned(&B, workspace_size);
+  tfctc::utils::alloc_aligned(&C, workspace_size);
+
+  set_random(A, workspace_size);
+  set_random(B, workspace_size);
+  set_random(C, workspace_size);
+
+  for (size_t i = 2; i < 62; i++)
   {
-    d1 = d2 = d3 = pow(2, i);
-
-    tfctc::utils::alloc_aligned(&A, d1 * d2);
-    tfctc::utils::alloc_aligned(&B, d2 * d3);
-    tfctc::utils::alloc_aligned(&C, d1 * d3);
-
-    set_random(A, d1 * d2);
-    set_random(B, d2 * d3);
-    set_random(C, d1 * d3);
-
-    const std::vector<size_t> lengthsA = { d1, d2 };
-    const std::vector<size_t> lengthsB = { d2, d3 };
-    const std::vector<size_t> lengthsC = { d1, d3 };
+    const std::vector<size_t> lengthsA = { i, i, i, i };
+    const std::vector<size_t> lengthsB = { i, i, i, i };
+    const std::vector<size_t> lengthsC = { i, i, i, i };
 
     auto tensorA = tfctc::Tensor<std::complex<float>>(lengthsA, A);
     auto tensorB = tfctc::Tensor<std::complex<float>>(lengthsB, B);
     auto tensorC = tfctc::Tensor<std::complex<float>>(lengthsC, C);
 
     std::vector<float> time(N);
-    for (uint i = 0; i < N; i++)
+    for (uint j = 0; j < N; j++)
     {
       auto t0 = std::chrono::high_resolution_clock::now();
-      tfctc::contract(tensorA, "ab", tensorB, "cb", tensorC, "ac");
+      tfctc::contract(tensorA, "abcd", tensorB, "ebcf", tensorC, "fade");
       auto t1 = std::chrono::high_resolution_clock::now();
-      time[i] = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+      time[j] = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
     }
 
-    std::cout << d1 << ';';
-    std::cout << *min_element(time.begin(), time.end()) << ';';
+    std::cout << int(i * i) << ',';
+    std::cout << *min_element(time.begin(), time.end()) << ',';
     std::cout << accumulate(time.begin(), time.end(), 0.0) * 1.0 / N << std::endl;
-
-    free(A);
-    free(B);
-    free(C);
   }
+
+  free(A);
+  free(B);
+  free(C);
 }
