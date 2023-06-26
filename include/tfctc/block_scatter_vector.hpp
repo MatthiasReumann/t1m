9d94eb35e35da0343bcc44466c9d3e87f403d47a
Matthias Reumann <matthias.reumann@tum.de>
2023-06-26 17:44:07 +0200

    Re-write block_scatter_vector

--- include/tfctc/block_scatter_vector.hpp
+++ include/tfctc/block_scatter_vector.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include "scatter_vector.hpp"
+#include "utils.hpp"
 
 namespace tfctc
 {
@@ -12,26 +13,40 @@ namespace tfctc
     public:
       BlockScatterVector(ScatterVector& scat, size_t b)
       {
-        size_t stride; // s, if constant; 0, if different strides
+        size_t i, stride; // s, if constant; 0, if different strides
         const size_t l = scat.size();
         const size_t size = std::ceil(l / static_cast<float>(b)); // ⌈l/b⌉
 
         this->bs.reserve(size);
 
-        for (int i = 0; i < l; i += b) // blocks
+        for (i = 0; i < size_t(l / b) * b; i += b) // blocks
         {
           stride = scat.at(i + 1) - scat.at(i);
-
-          for (int j = i; j < std::min(i + b, l) - 1; j++)
-          {
-            if (stride != scat.at(j + 1) - scat.at(j))
+          
+          for (size_t j = i + 2; j < i + b; j++) {
+            if ((scat.at(j) - scat.at(j - 1)) != stride)
             {
-              stride = 0; break;
+              stride = 0;
+              break;
             }
           }
-
           this->bs.push_back(stride);
         }
+        
+        stride = 0;
+        if (i + 1 < l)
+        {
+          stride = scat.at(i + 1) - scat.at(i);
+        
+          for (size_t j = i + 2; j < l; j++) {
+            if ((scat.at(j) - scat.at(j - 1)) != stride)
+            {
+              stride = 0;
+              break;
+            }
+          }
+        }
+        this->bs.push_back(stride);
       }
 
       size_t at(size_t i)
