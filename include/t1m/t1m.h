f68e319beb049bf3452cbf1b72cc2b65af6688c1
Matthias Reumann <matthias.reumann@tum.de>
2025-07-05 17:10:42 +0200

    Merge complex and real contract functions

--- include/t1m/t1m.h
+++ include/t1m/t1m.h
@@ -22,6 +22,10 @@ concept TensorScalarCompound = std::is_same_v<T, std::complex<float>> ||
 template <typename T>
 concept TensorScalar = TensorScalarArithmetic<T> || TensorScalarCompound<T>;
 
+template <typename T>
+using underlying_scalar_t =
+    std::conditional_t<TensorScalarCompound<T>, typename T::value_type, T>;
+
 enum memory_layout : std::uint8_t { row_major, col_major };
 
 template <TensorScalar T, std::size_t ndim> class tensor {
@@ -277,8 +281,7 @@ enum packing_label {
 };
 
 template <TensorScalar T, packing_label label,
-          typename U = std::conditional_t<TensorScalarCompound<T>,
-                                          typename T::value_type, T>>
+          typename U = underlying_scalar_t<T>>
 void pack_cell(const matrix_view& cell, const T* src, U* dest) {
   const std::size_t nrows = cell.nrows();
   const std::size_t ncols = cell.ncols();
@@ -340,8 +343,7 @@ void pack_cell(const matrix_view& cell, const T* src, U* dest) {
 }
 
 template <TensorScalar T, packing_label label,
-          typename U = std::conditional_t<TensorScalarCompound<T>,
-                                          typename T::value_type, T>>
+          typename U = underlying_scalar_t<T>>
 void pack_block(const matrix_view& block, const std::size_t length,
                 const T* src, U* dest) {
   const std::size_t nrows = block.nrows();
@@ -405,9 +407,7 @@ void pack_block(const matrix_view& block, const std::size_t length,
   }
 }
 
-template <TensorScalar T,
-          typename U = std::conditional_t<TensorScalarCompound<T>,
-                                          typename T::value_type, T>>
+template <TensorScalar T, typename U = underlying_scalar_t<T>>
 void unpack(const matrix_view& block, const U* src, T* dest) {
   const std::size_t nrows = block.nrows();
   const std::size_t ncols = block.ncols();
@@ -432,102 +432,14 @@ void unpack(const matrix_view& block, const U* src, T* dest) {
     }
   }
 }
-};  // namespace internal
 
-template <TensorScalarArithmetic T, std::size_t ndim_a, std::size_t ndim_b,
-          std::size_t ndim_c>
-void contract(const T alpha, const tensor<T, ndim_a>& a,
+template <TensorScalar T, std::size_t ndim_a, std::size_t ndim_b,
+          std::size_t ndim_c, typename U = underlying_scalar_t<T>>
+void contract(const U alpha, const tensor<T, ndim_a>& a,
               const std::string& labels_a, const tensor<T, ndim_b>& b,
-              const std::string& labels_b, const T beta, tensor<T, ndim_c>& c,
-              const std::string& labels_c,
-              const cntx_t* cntx = bli_gks_query_cntx()) {
-  using namespace t1m::internal;
-  using namespace t1m::bli;
-
-  const auto& [MR, NR, KP, MC, KC, NC] = get_block_sizes<T>(cntx);
-
-  const auto [bundle_a, bundle_b, bundle_c] =
-      get_index_bundles(labels_a, labels_b, labels_c);
-  const matrix_layout layout_a(a, bundle_a, MR, KP);
-  const matrix_layout layout_b(b, bundle_b, KP, NR);
-  const matrix_layout layout_c(c, bundle_c, MR, NR);
-
-  const matrix_view matr_a = layout_a.to_view();
-  const matrix_view matr_b = layout_b.to_view();
-  const matrix_view matr_c = layout_c.to_view();
-
-  const std::size_t space_size_a = MC * KC;
-  const std::size_t space_size_b = KC * NC;
-  const std::size_t space_size_c = MR * NR;
-  const std::size_t space_total = space_size_a + space_size_b + space_size_c;
-
-  T* space_a = static_cast<T*>(std::aligned_alloc(64, space_total * sizeof(T)));
-  if (!space_a) {
-    throw std::bad_alloc();
-  }
-  T* space_b = space_a + space_size_a;
-  T* space_c = space_b + space_size_b;
-
-  const std::size_t M = matr_a.nrows();
-  const std::size_t K = matr_a.ncols();
-  const std::size_t N = matr_b.ncols();
-
-  for (std::size_t j_c = 0; j_c < N; j_c += NC) {
-    const std::size_t nc_n = std::min(NC, N - j_c);
-
-    for (std::size_t p_c = 0; p_c < K; p_c += KC) {
-      const std::size_t k = std::min(KC, K - p_c);
-      const matrix_view view_b = matr_b.subview(p_c, j_c, k, nc_n);
-
-      std::fill(space_b, space_b + space_size_b, T(0));
-      pack_block<T, packing_label::B>(view_b, KC, b.data(), space_b);
-
-      for (size_t i_c = 0; i_c < M; i_c += MC) {
-        const std::size_t mc_m = std::min(MC, M - i_c);
-        const matrix_view view_a = matr_a.subview(i_c, p_c, mc_m, k);
-
-        std::fill(space_a, space_a + space_size_a, T(0));
-        pack_block<T, packing_label::A>(view_a, KC, a.data(), space_a);
-
-        for (size_t j_r = 0; j_r < nc_n; j_r += NR) {
-          const std::size_t n = std::min(NR, nc_n - j_r);
-          const std::size_t cci = j_c + j_r;
-
-          const T* sliver_b = space_b + KC * j_r;
-
-          for (size_t i_r = 0; i_r < mc_m; i_r += MR) {
-            const std::size_t m = std::min(MR, mc_m - i_r);
-            const std::size_t cri = i_c + i_r;
-
-            const T* sliver_a = space_a + i_r * KC;
-
-            const matrix_view view_c = matr_c.subview(cri, cci, m, n);
-
-            std::fill(space_c, space_c + space_size_c, T(0));
-
-            auxinfo_t data;
-            gemm_kernel<T>(m, n, k, &alpha, sliver_a, sliver_b, &beta, space_c,
-                           1, MR, &data, cntx);
-            unpack(view_c, space_c, c.data());
-          }
-        }
-      }
-    }
-  }
-
-  std::free(space_a);
-}
-
-template <TensorScalarCompound T, std::size_t ndim_a, std::size_t ndim_b,
-          std::size_t ndim_c, class U = typename T::value_type>
-void contract(const tensor<T, ndim_a>& a, const std::string& labels_a,
-              const tensor<T, ndim_b>& b, const std::string& labels_b,
-              tensor<T, ndim_c>& c, const std::string& labels_c,
-              const cntx_t* cntx = bli_gks_query_cntx()) {
-  using namespace t1m::internal;
-  using namespace t1m::bli;
-
-  const auto& [MR, NR, KP, MC, KC, NC] = get_block_sizes<T>(cntx);
+              const std::string& labels_b, const U beta, tensor<T, ndim_c>& c,
+              const std::string& labels_c, const cntx_t* cntx) {
+  const auto& [MR, NR, KP, MC, KC, NC] = bli::get_block_sizes<T>(cntx);
 
   const auto [bundle_a, bundle_b, bundle_c] =
       get_index_bundles(labels_a, labels_b, labels_c);
@@ -551,54 +463,96 @@ void contract(const tensor<T, ndim_a>& a, const std::string& labels_a,
   U* space_b = space_a + space_size_a;
   U* space_c = space_b + space_size_b;
 
-  const U alpha = U(1);
-  const U beta = U(0);
-
   const std::size_t M = matr_a.nrows();
   const std::size_t K = matr_a.ncols();
   const std::size_t N = matr_b.ncols();
 
-  for (std::size_t j_c = 0; j_c < N; j_c += NC) {
-    const std::size_t nc_n = std::min(NC, N - j_c);
+  if constexpr (TensorScalarArithmetic<T>) {
+    for (std::size_t j_c = 0; j_c < N; j_c += NC) {
+      const std::size_t nc_n = std::min(NC, N - j_c);
 
-    for (std::size_t p_c = 0; p_c < K; p_c += KC / 2) {
-      const std::size_t k = std::min(KC / 2, K - p_c);
-      const dim_t k_real = k * 2;
+      for (std::size_t p_c = 0; p_c < K; p_c += KC) {
+        const std::size_t k = std::min(KC, K - p_c);
+        const matrix_view view_b = matr_b.subview(p_c, j_c, k, nc_n);
 
-      const matrix_view view_b = matr_b.subview(p_c, j_c, k, nc_n);
+        std::fill(space_b, space_b + space_size_b, T(0));
+        pack_block<T, packing_label::B>(view_b, KC, b.data(), space_b);
 
-      std::fill(space_b, space_b + space_size_b, U(0));
-      pack_block<T, packing_label::B>(view_b, KC, b.data(), space_b);
+        for (size_t i_c = 0; i_c < M; i_c += MC) {
+          const std::size_t mc_m = std::min(MC, M - i_c);
+          const matrix_view view_a = matr_a.subview(i_c, p_c, mc_m, k);
 
-      for (size_t i_c = 0; i_c < M; i_c += MC / 2) {
-        const std::size_t mc_m = std::min(MC / 2, M - i_c);
-        const std::size_t mc_m_real = 2 * mc_m;
+          std::fill(space_a, space_a + space_size_a, T(0));
+          pack_block<T, packing_label::A>(view_a, KC, a.data(), space_a);
 
-        const matrix_view view_a = matr_a.subview(i_c, p_c, mc_m, k);
+          for (size_t j_r = 0; j_r < nc_n; j_r += NR) {
+            const std::size_t n = std::min(NR, nc_n - j_r);
+            const std::size_t cci = j_c + j_r;
 
-        std::fill(space_a, space_a + space_size_a, U(0));
-        pack_block<T, packing_label::A>(view_a, KC, a.data(), space_a);
+            const T* sliver_b = space_b + KC * j_r;
 
-        for (size_t j_r = 0; j_r < nc_n; j_r += NR) {
-          const std::size_t n = std::min(NR, nc_n - j_r);
-          const std::size_t cci = j_c + j_r;
-          const U* sliver_b = space_b + KC * j_r;
+            for (size_t i_r = 0; i_r < mc_m; i_r += MR) {
+              const std::size_t m = std::min(MR, mc_m - i_r);
+              const std::size_t cri = i_c + i_r;
 
-          for (size_t i_r = 0; i_r < mc_m_real; i_r += MR) {
-            const std::size_t m = std::min(MR, mc_m_real - i_r);
-            const std::size_t cri = i_c + (i_r / 2);
+              const T* sliver_a = space_a + i_r * KC;
 
-            const U* sliver_a = space_a + i_r * KC;
+              const matrix_view view_c = matr_c.subview(cri, cci, m, n);
 
-            const matrix_view view_c = matr_c.subview(cri, cci, m / 2, n);
+              std::fill(space_c, space_c + space_size_c, T(0));
 
-            std::fill(space_c, space_c + space_size_c, U(0));
+              auxinfo_t data;
+              bli::gemm_kernel<T>(m, n, k, &alpha, sliver_a, sliver_b, &beta,
+                                  space_c, 1, MR, &data, cntx);
+              unpack(view_c, space_c, c.data());
+            }
+          }
+        }
+      }
+    }
+  } else {  // Use 1M 5-Loop Approach.
+    for (std::size_t j_c = 0; j_c < N; j_c += NC) {
+      const std::size_t nc_n = std::min(NC, N - j_c);
 
-            auxinfo_t data;
-            gemm_kernel<T>(m, n, k_real, &alpha, sliver_a, sliver_b, &beta,
-                           space_c, 1, MR, &data, cntx);
+      for (std::size_t p_c = 0; p_c < K; p_c += KC / 2) {
+        const std::size_t k = std::min(KC / 2, K - p_c);
+        const dim_t k_real = k * 2;
 
-            unpack(view_c, space_c, c.data());
+        const matrix_view view_b = matr_b.subview(p_c, j_c, k, nc_n);
+
+        std::fill(space_b, space_b + space_size_b, U(0));
+        pack_block<T, packing_label::B>(view_b, KC, b.data(), space_b);
+
+        for (size_t i_c = 0; i_c < M; i_c += MC / 2) {
+          const std::size_t mc_m = std::min(MC / 2, M - i_c);
+          const std::size_t mc_m_real = 2 * mc_m;
+
+          const matrix_view view_a = matr_a.subview(i_c, p_c, mc_m, k);
+
+          std::fill(space_a, space_a + space_size_a, U(0));
+          pack_block<T, packing_label::A>(view_a, KC, a.data(), space_a);
+
+          for (size_t j_r = 0; j_r < nc_n; j_r += NR) {
+            const std::size_t n = std::min(NR, nc_n - j_r);
+            const std::size_t cci = j_c + j_r;
+            const U* sliver_b = space_b + KC * j_r;
+
+            for (size_t i_r = 0; i_r < mc_m_real; i_r += MR) {
+              const std::size_t m = std::min(MR, mc_m_real - i_r);
+              const std::size_t cri = i_c + (i_r / 2);
+
+              const U* sliver_a = space_a + i_r * KC;
+
+              const matrix_view view_c = matr_c.subview(cri, cci, m / 2, n);
+
+              std::fill(space_c, space_c + space_size_c, U(0));
+
+              auxinfo_t data;
+              bli::gemm_kernel<T>(m, n, k_real, &alpha, sliver_a, sliver_b,
+                                  &beta, space_c, 1, MR, &data, cntx);
+
+              unpack(view_c, space_c, c.data());
+            }
           }
         }
       }
@@ -606,5 +560,25 @@ void contract(const tensor<T, ndim_a>& a, const std::string& labels_a,
   }
 
   std::free(space_a);
+}
+};  // namespace internal
+
+template <TensorScalarArithmetic T, std::size_t ndim_a, std::size_t ndim_b,
+          std::size_t ndim_c>
+void contract(const T alpha, const tensor<T, ndim_a>& a,
+              const std::string& labels_a, const tensor<T, ndim_b>& b,
+              const std::string& labels_b, const T beta, tensor<T, ndim_c>& c,
+              const std::string& labels_c,
+              const cntx_t* cntx = bli_gks_query_cntx()) {
+  internal::contract(alpha, a, labels_a, b, labels_b, beta, c, labels_c, cntx);
+}
+
+template <TensorScalarCompound T, std::size_t ndim_a, std::size_t ndim_b,
+          std::size_t ndim_c, class U = typename T::value_type>
+void contract(const tensor<T, ndim_a>& a, const std::string& labels_a,
+              const tensor<T, ndim_b>& b, const std::string& labels_b,
+              tensor<T, ndim_c>& c, const std::string& labels_c,
+              const cntx_t* cntx = bli_gks_query_cntx()) {
+  internal::contract(U(1), a, labels_a, b, labels_b, U(0), c, labels_c, cntx);
 }
 };  // namespace t1m
